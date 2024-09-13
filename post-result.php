780d0bba081ff4276b809241d0ca950d80171f6f
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-09-12 22:34:39 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -4,15 +4,21 @@ include "view-header.php";
 ?>
     <h1>Post Result</h1>
 <?php
+echo getDisplay();
+include "view-footer.php";
+
+function getDisplay(){
 if (isset($_POST['my-name'])) {
   ?>
   <p>The value sent is: </p>
-  <?
+  <?php
   echo $_POST['my-name'];
 } else {
   ?>
   <p>Nothing posted to the page.</p>
-  <?
+  <?php
 }
 include "view-footer.php";
+?>
+}
 ?>
